256261c1ab6685ee3912ac9c3e8655cdd7f3f594
AnnaCarolinaSG <annacarolinagarofolo@gmail.com>
2025-04-03 16:58:50 -0300

    Estruturação inicial da tela principal

--- avaliaquick/urls.py
+++ avaliaquick/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from avaliaquick.views import index
+from avaliaquick.views import index, avaliacao
 
 urlpatterns = [
     path('', index),
+    path('avaliacao/', avaliacao)
 ]
--- avaliaquick/views.py
+++ avaliaquick/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'avaliaquick/index.html')
 
+def avaliacao(request):
+    return render(request, 'avaliaquick/avaliacao.html')
+
 # Create your views here.
