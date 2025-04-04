662b08553c52cf9144556eaaca65264ff2698433
brian <brian@unicamp.br>
2025-04-04 16:47:02 -0300

    Linkagem de botões e páginas

--- avaliaquick/urls.py
+++ avaliaquick/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from avaliaquick.views import index, avaliacao
+from avaliaquick.views import index, avaliacao, perfil, lista, anteriores
 
 urlpatterns = [
     path('', index),
-    path('avaliacao/', avaliacao)
+    path('avaliacao/', avaliacao),
+    path('perfil', perfil),
+    path('lista-pesquisadores', lista),
+    path('avaliacoes-anteriores', anteriores)
 ]
--- avaliaquick/views.py
+++ avaliaquick/views.py
@@ -6,4 +6,13 @@ def index(request):
 def avaliacao(request):
     return render(request, 'avaliaquick/avaliacao.html')
 
+def perfil(request):
+    return render(request, 'avaliaquick/perfil.html')
+
+def lista(request):
+    return render(request, 'avaliaquick/lista-pesquisadores.html')
+
+def anteriores(request):
+    return render(request, 'avaliaquick/avaliacoes-anteriores.html')
+
 # Create your views here.
