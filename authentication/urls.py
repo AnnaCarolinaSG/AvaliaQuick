06046bc769c08866348a21a50c111d3ec2a60310
BrianCosta2004 <brianhcosta@gmail.com>
2025-04-18 18:42:58 -0300

    Correção do Logout

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,11 +4,10 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.urls import path
-from .views import login_view, register_user
-from django.contrib.auth.views import LogoutView
+from .views import login_view, register_user, logout_view
 
 urlpatterns = [
     path('', login_view, name="login"),
     path('register/', register_user, name="register"),
-    path("logout/", LogoutView.as_view(), name="logout"),
+    path("logout/", logout_view, name="logout"),
 ]
