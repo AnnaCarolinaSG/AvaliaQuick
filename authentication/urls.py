9e266d1904e64095ed7906e4babd36721454c626
BrianCosta2004 <brianhcosta@gmail.com>
2025-07-01 18:35:44 -0300

    Reset de Senha - V1.3

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,10 +4,16 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import login_view, register_user, logout_view
 
 urlpatterns = [
     path('', login_view, name="login"),
     path('register/', register_user, name="register"),
     path("logout/", logout_view, name="logout"),
+
+    path('senha/resetar/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'), name='password_reset'),
+    path('senha/resetar/enviado/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
+    path('senha/resetar/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('senha/resetar/concluido/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
 ]
